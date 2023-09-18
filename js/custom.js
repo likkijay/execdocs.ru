e6e05d1623b8de3187b7e147094b20af96325f6b
likkijay <likkijay@gmail.com>
2023-09-18 11:00:55 +0500

    Execdocs website v 1.0.0

---
+++ js/custom.js
@@ -0,0 +1,61 @@
+//service section owl carousel
+$(".service_owl-carousel").owlCarousel({
+    autoplay: true,
+    center: true,
+    nav: true,
+    loop: true,
+    margin: 0,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 3,
+        },
+        991: {
+            items: 3
+        }
+    }
+});
+
+// owl carousel slider js
+var owl = $('.portfolio_carousel').owlCarousel({
+    loop: true,
+    margin: 15,
+    dots: false,
+    center: true,
+    autoplay: true,
+    navText: [
+        '<i class="fa fa-arrow-left" aria-hidden="true"></i>',
+        '<i class="fa fa-arrow-right" aria-hidden="true"></i>'
+    ],
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            center: false,
+            items: 1,
+            margin: 0
+        },
+        576: {
+            items: 2
+        },
+        991: {
+            center: true,
+            items: 3
+        }
+    }
+})
+
+
+// owl.owlcarousel2_filter
+
+$('.owl-filter-bar').on('click', '.item', function (e) {
+    var $items = $('.owl-filter-bar a')
+    var $item = $(this);
+    var filter = $item.data('owl-filter')
+    $items.removeClass("active");
+    $item.addClass("active");
+    owl.owlcarousel2_filter(filter);
+
+    e.preventDefault();
+})
